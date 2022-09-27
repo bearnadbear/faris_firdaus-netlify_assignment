7b30f0ae3963e67e9506d87438329ef39a02b367
Faris Firdaus Ariputra <punyanyaarii@gmail.com>
2022-09-28 01:48:44 +0700

    update index.html and add file tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// File to enable tailwindcss intellisense
+
+module.exports = {
+    theme: {
+      screens: {
+        'tablet': '320px',
+        // => @media (min-width: 320px) { ... }
+  
+        'laptop': '1080px',
+        // => @media (min-width: 1080px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    }
+  }
